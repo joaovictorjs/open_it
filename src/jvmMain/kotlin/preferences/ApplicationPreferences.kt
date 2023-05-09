55f68e8c0e8e4f76c7fe4d36a7d26901a5dbd356
joaovictorjs <skai7ami@gmail.com>
2023-05-09 10:57:38 -0300

    trazer versão atual(resolvido)

--- src/jvmMain/kotlin/preferences/ApplicationPreferences.kt
+++ src/jvmMain/kotlin/preferences/ApplicationPreferences.kt
@@ -5,32 +5,28 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.window.FrameWindowScope
-import java.io.FileInputStream
-import java.util.Properties
 import java.util.prefs.Preferences
 
 class ApplicationPreferences {
     companion object {
         lateinit var windowScope: FrameWindowScope
-        private val name = this::class.simpleName
-        private val preferences = Preferences.userRoot().node(name)
+        private val _name = ApplicationPreferences::javaClass.name
+        private val preferences = Preferences.userRoot().node(_name)
 
-        //TODO: resolver 'gradle.properties not found' após gerar release
-//        private val gradleProps = Properties().apply {
-//            load(FileInputStream("gradle.properties"))
-//        }
-//        private var _appVersion by mutableStateOf(gradleProps["application.version"] as String)
-
-        private var _appVersion by mutableStateOf("1.0.0")
+        private var _appVersion by mutableStateOf("")
 
         val appVersion by derivedStateOf { _appVersion }
-        private var _useLightTheme by mutableStateOf(preferences.getBoolean("use-light-theme", true))
+        private var _useLightTheme by mutableStateOf(preferences.node(_name).getBoolean("use-light-theme", true))
         val useLightTheme by derivedStateOf { _useLightTheme }
 
         fun changeTheme(useLightTheme: Boolean) {
-            _useLightTheme = useLightTheme
-            preferences.node(name).putBoolean("use-light-theme", useLightTheme)
+            preferences.node(_name).putBoolean("use-light-theme", useLightTheme)
             preferences.flush()
+            _useLightTheme = useLightTheme
+        }
+
+        fun version(value: String?) {
+            _appVersion = (value ?: "development version").replace("version=", "")
         }
     }
 }
